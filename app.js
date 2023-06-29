5f6b34eee4d03bf22fd55717f14e854231cd9030
buraktr03 <turanburak@gmail.com>
2023-06-29 20:53:55 +0300

    final

--- app.js
+++ app.js
@@ -1,44 +1,58 @@
 const ssn = document.getElementById("ssn");
 const validate = document.getElementById("btn");
+const reset = document.getElementById("reset-btn");
+const result = document.getElementById("result");
 
-function validateSsn(number) {
-  let value = validate.value;
+function validateSsn() {
+  let value = ssn.value;
   // Remove non-numeric characters
   value = value.replace(/\D/g, "");
-  inputElement.value = value; // Update input value with numeric characters only
+  ssn.value = value; // Update input value with numeric characters only
 
-  let numberStr = number.toString();
+  let numberStr = value.toString();
 
   if (numberStr.length !== 9) {
-    return "Please enter a nine-digit number.";
+    result.innerHTML = "Please enter a nine-digit number.";
+    return;
   }
 
   let firstPart = Number(numberStr.slice(0, 3));
   let secondPart = Number(numberStr.slice(3, 5));
   let thirdPart = Number(numberStr.slice(5));
 
-  let trueNumber = `<div class="card">
-  <div class="card-body bg-success">
-  Entered SSN(${firstPart}-${secondPart}-${thirdPart}) is valid!
-  </div>
-</div>`;
-  let falseNumber = `<div class="card">
-  <div class="card-body bg-danger">
-  Entered SSN(${firstPart}-${secondPart}-${thirdPart}) is not valid!
-  </div>
-</div>`;
+  let trueNumber = `
+    <div class="card">
+      <div class="card-body bg-success">
+        Entered SSN (${firstPart}-${secondPart}-${thirdPart}) is valid!
+      </div>
+    </div>
+  `;
+
+  let falseNumber = `
+    <div class="card">
+      <div class="card-body bg-danger">
+        Entered SSN (${firstPart}-${secondPart}-${thirdPart}) is not valid!
+      </div>
+    </div>
+  `;
 
   if (
     (firstPart >= 900 && firstPart <= 999) ||
-    firstPart === "000" ||
-    firstPart === "666" ||
-    secondPart >= 1 ||
-    secondPart <= 99 ||
-    thirdPart >= 1 ||
-    thirdPart <= 9999
+    firstPart === 0 ||
+    firstPart === 666 ||
+    secondPart < 1 ||
+    secondPart > 99 ||
+    thirdPart < 1 ||
+    thirdPart > 9999
   ) {
-    return falseNumber;
+    result.innerHTML = falseNumber;
   } else {
-    return trueNumber;
+    result.innerHTML = trueNumber;
   }
 }
+
+validate.addEventListener("click", validateSsn);
+reset.addEventListener("click", function() {
+  ssn.value = "";
+  result.innerHTML = "";
+});
